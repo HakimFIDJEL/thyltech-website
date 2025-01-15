383a9dd65cda05efc02ed84206997b36b14ba7e2
HakimFIDJEL <hakimfidjel.pro@gmail.com>
2025-01-15 16:59:14 +0100

    minor change

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -15,7 +15,7 @@ export function SectionTitle({
     <div className={cn("py-4", className)}>
       <div className="relative py-2">
         <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-gray-300" />
+          <div className="w-full border-t border-border" />
         </div>
 
         <div className="relative flex">
