c3588db8d85b49655d59cb950e90c7d36d0e201a
HakimFIDJEL <hakim.fidjel@gmail.com>
2025-01-24 11:32:49 +0100

    toast added

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { TopButton } from "./components/TopButton";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,6 +35,7 @@ export default function RootLayout({
           <Footer />
 
           <TopButton />
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -3,37 +3,53 @@ import { SectionTitle } from "../components/SectionTitle";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Download, Linkedin, Mail, Send, Loader2  } from "lucide-react";
+import { Download, Linkedin, Mail, Send, Loader2 } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { ContactItem } from "../components/ContactItem";
 import { FadeInComponent } from "../components/FadeInComponent";
 import { FullSeparator } from "../components/FullSeparator";
 import Link from "next/link";
 import { useState } from "react";
+import { useToast } from "@/hooks/use-toast"
 
 export function Contact() {
-
   const [loading, setLoading] = useState(false);
+  const { toast } = useToast()
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setLoading(true);
+
+    // TODO : Implement the contact form
+    toast({
+      description: "Method not implemented",
+      variant: "destructive",
+      duration: 3000
+    });
+
+    setLoading(false);
+
+  }
 
   return (
     <section
       className="relative overflow-hidden bg-background flex items-center justify-end flex-col"
       id="contact"
     >
-        <SectionTitle
-          title="Contactez-nous"
-          subtitle="Nous sommes à votre écoute pour toute question ou suggestion"
-        />
-        <FullSeparator />
+      <SectionTitle
+        title="Contactez-nous"
+        subtitle="Nous sommes à votre écoute pour toute question ou suggestion"
+      />
+      <FullSeparator />
       <div className="container px-4 md:px-6 border-l border-r border-border  pt-8 pb-16">
-
         <div className="flex flex-col md:flex-row gap-8">
           <FadeInComponent className="w-full md:w-1/2 space-y-6">
             <p className="text-medium font-semibold text-center">
               Notre formulaire de contact
             </p>
             <Separator />
-            <form className="space-y-4" onSubmit={() => setLoading(true)}>
+            <form className="space-y-4" onSubmit={onSubmit}>
               <div>
                 <label
                   htmlFor="name"
@@ -50,7 +66,12 @@ export function Contact() {
                 >
                   Email
                 </label>
-                <Input id="email" type="email" placeholder="votre@email.com" required />
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="votre@email.com"
+                  required
+                />
               </div>
               <div>
                 <label
@@ -67,16 +88,27 @@ export function Contact() {
                   required
                 />
               </div>
-              <Button type="submit" className="w-full flex items-center justify-center gap-2" disabled={loading}>
-                <Send className={`h-4 w-4 ${loading ? 'hidden' : 'block'}`} />
-                <Loader2 className={`animate-spin h-4 w-4 ${loading ? 'block' : 'hidden'}`} />
+              <Button
+                type="submit"
+                className="w-full flex items-center justify-center gap-2"
+                disabled={loading}
+              >
+                <Send className={`h-4 w-4 ${loading ? "hidden" : "block"}`} />
+                <Loader2
+                  className={`animate-spin h-4 w-4 ${
+                    loading ? "block" : "hidden"
+                  }`}
+                />
                 Envoyer
               </Button>
             </form>
           </FadeInComponent>
 
           <Separator orientation="vertical" className="h-100 hidden md:block" />
-          <Separator orientation="horizontal" className="w-full block md:hidden" />
+          <Separator
+            orientation="horizontal"
+            className="w-full block md:hidden"
+          />
 
           <FadeInComponent className="w-full md:w-1/2 space-y-6">
             <p className="text-medium font-semibold text-center">
@@ -103,13 +135,11 @@ export function Contact() {
 
               <Link href="/Thyltech_Plaquette.pdf" target="_blank" download>
                 <Button className="flex items-center justify-center w-full gap-2 mt-4">
-                  <Download className="h-4 w-4"/>
+                  <Download className="h-4 w-4" />
                   Téléchargez notre plaquette
                 </Button>
               </Link>
             </div>
-
-          
           </FadeInComponent>
         </div>
       </div>
